8f99d369fffcf0d0a9b1b409e607173a44811700
Ryan Calingo <gryan.calingo@gmail.com>
2023-12-06 15:01:49 -0500

    Simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import "./App.css";
+import styles from "./App.module.css";
 
-const App = () => (
-	<>
-		<div></div>
-	</>
-);
+function App() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.titleCtn}>
+				<h1>ROCK, PAPER, SCISSORS</h1>
+				<p>React Typescript Game!</p>
+			</div>
+			<ScoreAndResults />
+			<ChooseAndPlay />
+		</div>
+	);
+}
 
 export default App;
